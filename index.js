99d12db6414f9379a2bbb020bed10d82df8d851f
anissalopez <anissa965@gmail.com>
2023-05-30 14:21:43 -0400

    context lab

--- index.js
+++ index.js
@@ -9,6 +9,73 @@
  for you to use if you need it!
  */
 
+function createEmployeeRecord(array){
+    let employeeRecord = {
+    firstName: array[0],
+    familyName: array[1],
+    title: array[2],
+    payPerHour: array[3],
+    timeInEvents: [],
+    timeOutEvents: []
+
+}
+    return employeeRecord;
+       
+}
+
+
+function createEmployeeRecords(array){
+    return array.map(function (elements){
+        return createEmployeeRecord(elements)
+    })
+}
+
+function createTimeInEvent(date){
+  let splitDate = date.split(" ");
+  this.timeInEvents.push({type: "TimeIn", hour: (+splitDate[1]), date: splitDate[0]});
+  return this;
+}
+
+function createTimeOutEvent(date){
+    let splitDate = date.split(" ");
+    this.timeOutEvents.push({type: "TimeOut", hour: (+splitDate[1]), date: splitDate[0]});
+    return this;
+  }
+
+function hoursWorkedOnDate(date){
+
+    let timeInEvent = this.timeInEvents.find(function (element){
+        return element.date === date
+    })
+    let timeOutEvent = this.timeOutEvents.find(function (element){
+        return element.date === date
+    })
+
+    return (timeOutEvent.hour - timeInEvent.hour)/100
+
+}
+
+function wagesEarnedOnDate(date){
+ return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+}
+
+function findEmployeeByFirstName (srcArray, firstName){
+    let matchingObject = srcArray.find(function (element) {
+      return element.firstName === firstName;
+    })
+  return matchingObject;
+  }
+
+function calculatePayroll(array){
+    
+    let totalPay = array.reduce(function (accumulator, array){
+        return accumulator + allWagesFor.apply(array);
+    }, 0);
+    return totalPay;
+}
+
+
+
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
